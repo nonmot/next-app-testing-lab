7790266c58c2b8e973eb185a50318f7bed56a91b
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-10-07 20:58:10 +0900

    test for Button component

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,7 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { Button } from "./Button";
 
-test("renders button", () => {
-  render(<Button label="Hello" onClick={() => {}} />)
-  expect(screen.getByText("Hello")).toBeInTheDocument();
+test("正しくラベルが表示されるか", () => {
+  render(<Button label="Click me" onClick={() => {}} />)
+  expect(screen.getByText('Click me')).toBeInTheDocument();
+})
+
+test("クリックされた時にハンドラが呼ばれるか", () => {
+  const handleClick = jest.fn();
+  render(<Button label="Click me" onClick={handleClick} />)
+
+  screen.getByRole('button').click();
+  expect(handleClick).toHaveBeenCalledTimes(1);
 })
